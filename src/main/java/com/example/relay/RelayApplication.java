11d0632352540884126342d4e3605c2f0d1a817f
Dhruv Kushwaha <dhruvcodes23@gmail.com>
2026-09-05 13:20:33 +0530

    add postgres testing setup

--- src/main/java/com/example/relay/RelayApplication.java
+++ src/main/java/com/example/relay/RelayApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RelayApplication {
 
-	public static void main(String[] args) {
+	static void main(String[] args) {
 		SpringApplication.run(RelayApplication.class, args);
 	}
 
